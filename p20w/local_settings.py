92a52fc5d717082b065a2110a03bd6346cc8f0e7
David Grenier <me@davidgrenier.com>
2018-11-21 11:33:20 -0500

    Added local_settings file

---
+++ p20w/local_settings.py
@@ -0,0 +1,17 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
+    }
+}
