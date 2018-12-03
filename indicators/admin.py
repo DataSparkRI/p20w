f8e5c92ef06e2f6f3036de2fa572e38d71896a4c
David Grenier <me@davidgrenier.com>
2018-12-03 16:45:56 -0500

    Fixed bug with indicator admin

--- indicators/admin.py
+++ indicators/admin.py
@@ -7,7 +7,7 @@ class QuestionAdmin(SortableAdminMixin, admin.ModelAdmin):
 	list_display = ('name', 'question', 'get_category')
 	list_filter = ['category']
 	fieldsets = [
-		(None, 			{'fields': [('name', 'category'), 'sort_order', 'question', 'text']}), 
+		(None, 			{'fields': [('name', 'category'), 'question', 'text']}), 
 		('Dashboard', 	{'fields': ['dashboard_embed', ('aspect_ratio_width', 'aspect_ratio_height')]}), 
 		('Pop stat', 	{'fields': ['pop_stat', 'pop_stat_label', 'pop_stat_explainer']})
 		]
